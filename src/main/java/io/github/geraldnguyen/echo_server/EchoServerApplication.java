8efc247f404d08dc7f306e67c1aeb56c699d1e95
Gerald Nguyen <ngthehuy@gmail.com>
2022-11-19 17:48:56 +0800

    allow CORS to /echo

--- src/main/java/io/github/geraldnguyen/echo_server/EchoServerApplication.java
+++ src/main/java/io/github/geraldnguyen/echo_server/EchoServerApplication.java
@@ -3,6 +3,9 @@ package io.github.geraldnguyen.echo_server;
 import nguyen.gerald.echo.EnableEchoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEchoController
@@ -10,6 +13,16 @@ public class EchoServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchoServerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/echo");
+			}
+		};
 	}
 
 }
